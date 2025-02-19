9447b90eae8a7bd930e9958b272051d9f225f475
kader009 <kadermollarasel000@gmail.com>
2025-02-19 15:01:03 +0600

    product route create and define

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import express from 'express';
+import cors from 'cors'
+import { ProductRouter } from './app/modules/products/product.routes';
 const app = express();
 
+
+// middileware
 app.use(express.json());
+app.use(cors())
+
+// routes
+app.use('/api/v1/products', ProductRouter)
 
 app.get('/', (req, res) => {
   res.send({
