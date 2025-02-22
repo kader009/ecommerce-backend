09ddd71b0ca55105bf303da078b1e421ce9db8bf
kader009 <kadermollarasel000@gmail.com>
2025-02-22 11:20:38 +0600

    route with admin issue solve

--- src/app/middleware/admin.middleware.ts
+++ src/app/middleware/admin.middleware.ts
@@ -2,28 +2,27 @@ import { Request, Response, NextFunction } from 'express';
 import { JwtPayload } from 'jsonwebtoken';
 
 interface AuthenticatedRequest extends Request {
-  decoded?: JwtPayload; 
+  decoded?: JwtPayload;
 }
 
 export const isAdmin = (
   req: AuthenticatedRequest,
   res: Response,
   next: NextFunction
-) => {
+): void => {
   if (req.decoded && typeof req.decoded !== 'string') {
     const userRole = req.decoded.role;
 
     if (userRole !== 'admin') {
-      return res.status(403).json({
+      res.status(403).json({
         success: false,
         message: 'You do not have access to perform this action',
       });
     }
 
     next();
-    
   } else {
-    return res.status(401).json({
+    res.status(401).json({
       success: false,
       message: 'Unauthorized: No token provided or invalid token',
     });
